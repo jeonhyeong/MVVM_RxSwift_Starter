6e5e9fed3ed2ff320248477f26e3fbc048fe451e
master <>
2019-07-14 01:29:57 +0900

    Implement search

--- Networking/ViewController.swift
+++ Networking/ViewController.swift
@@ -23,16 +23,23 @@ class ViewController: UIViewController {
                 cell.textLabel?.text = repository.repoName
                 cell.detailTextLabel?.text = repository.repoURL
             }
-            .addDisposableTo(disposeBag)
+            .disposed(by: disposeBag)
         
+        searchBar.rx.text.orEmpty
+        .bind(to: viewModel.searchText)
+        .disposed(by: disposeBag)
         
+        viewModel.data.asDriver()
+        .map{ "\($0.count) Repositories" }
+        .drive(navigationItem.rx.title)
+        .disposed(by: disposeBag)
     }
     
     func configureSearchController() {
         searchController.obscuresBackgroundDuringPresentation = false
         searchBar.showsCancelButton = true
-        searchBar.text = "NavdeepSinghh"
-        searchBar.placeholder = "Enter GitHub ID, e.g., \"NavdeepSinghh\""
+        searchBar.text = "Jeonhyeong"
+        searchBar.placeholder = "Enter GitHub ID, e.g., \"Jeonhyeong\""
         tableView.tableHeaderView = searchController.searchBar
         definesPresentationContext = true
     }
--- Networking/ViewModel.swift
+++ Networking/ViewModel.swift
@@ -5,12 +5,46 @@ import RxCocoa
 
 class ViewModel {
     
-    let searchText = Variable("")
+    let searchText = BehaviorSubject(value: "")
     
     lazy var data: Driver<[Repository]> = {
         
-        return Observable.of([Repository]()).asDriver(onErrorJustReturn: [])
+        return self.searchText.asObservable()
+        .throttle(0.3, scheduler: MainScheduler.instance)
+        .distinctUntilChanged()
+        .flatMapLatest(ViewModel.repositoriesBy)
+        .asDriver(onErrorJustReturn: [])
     }()
+    
+    
+    static func repositoriesBy(_ githubID: String) -> Observable<[Repository]> {
+        guard !githubID.isEmpty,
+        let url = URL(string: "https://api.github.com/users/\(githubID)/repos") else {
+            return Observable.just([])
+        }
+        
+        return URLSession.shared.rx.json(url: url)
+        .retry(3)
+        .map(parse)
+        .catchErrorJustReturn([])
+    }
+    
+    static func parse(json: Any) -> [Repository] {
+        guard let items = json as? [[String: Any]] else {
+            return []
+        }
+        
+        var repositories = [Repository]()
+        
+        items.forEach {
+            guard let repoName = $0["name"] as? String,
+                let repoURL = $0["html_url"] as? String else {
+                    return
+            }
+            repositories.append(Repository(repoName: repoName, repoURL: repoURL))
+        }
+        return repositories
+    }
 }
 
 
